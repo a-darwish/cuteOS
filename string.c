929e0a83d19d9b89012be61d8c039b354bd59830
Ahmed S. Darwish <darwish.07@gmail.com>
2009-09-24 19:43:26 +0300

    Add an optimized memset() implementation.

--- string.c
+++ string.c
@@ -19,6 +19,7 @@ int strlen(const char *str)
 		;
 	return tmp - str;
 }
+
 void strncpy(char *dst, const char *src, int n)
 {
 	char *tmp = dst;
@@ -33,8 +34,11 @@ void strncpy(char *dst, const char *src, int n)
 }
 
 /*
- * Optimized `movsq' memcpy
+ * Optimized mem* methods
+ * For more speed, we should let movsq and stosq read
+ * arguments (source) be 8 byte aligned.
  */
+
 void memcpy(void *dst, void *src, int len)
 {
 	int tmp;
@@ -48,4 +52,29 @@ void memcpy(void *dst, void *src, int len)
 	    :
 	    : "S"(src), "D"(dst), [len] "c"(len), [tmp] "r"(tmp)
 	    : "memory");
+}
+
+char *memset(void *dst, int ch, int len)
+{
+	unsigned long tmp;
+	unsigned long c = ch;
+	unsigned long l = len;
+
+	ch &= 0xff;
+	asm("cld;"
+	    "mov %[len], %[tmp];"
+	    "and $7, %[len];"
+	    "rep  stosb;"
+	    "mov %[tmp], %[len];"
+	    "shr $3, %[len];"
+	    /* Copy the right-most byte to the rest 7 bytes
+	     * value extracted by: 0xffffffffffffffff/0xff */
+	    "mov  %[ch], %[tmp];"
+	    "mov  $0x0101010101010101, %[ch];"
+	    "mul  %[tmp];"
+	    "rep  stosq;"
+	    :
+	    : "D"(dst), [ch] "a"(c), [len] "c"(l), [tmp] "r"(tmp)
+	    : "memory");
+	return dst;
 }
