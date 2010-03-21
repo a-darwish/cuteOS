d43c2fec593f1654bbf68bd9a27bd494f2fb336f
Ahmed S. Darwish <darwish.07@gmail.com>
2010-03-21 03:09:23 +0200

    Program PIT's timer-0 as oneshot or monotonic mode.
    This is one of scheduler's early building blocks.

--- dev/pit.c
+++ dev/pit.c
@@ -1,7 +1,7 @@
 /*
  * i8253/i8254-compatible PIT
  *
- * Copyright (C) 2009 Ahmed S. Darwish <darwish.07@gmail.com>
+ * Copyright (C) 2009-2010 Ahmed S. Darwish <darwish.07@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -10,13 +10,23 @@
  * Check Intel's "82C54 CHMOS Programmable Interval Timer" datasheet
  * for more details.
  *
- * In a PC compatible machine, port 0x61 bit 0 controls the PIT's GATE
- * 2: the gate input pin of counter 2. Bit 5 is the OUT 2 pin: the output
- * pin of counter 2. Bit 1 is an input pin to a glue AND logic with the
- * OUT 2 pin, outputting counter 2 ticks to the system speaker if set.
+ * The PIT contains three timers (counters). Each timer is attached to a
+ * GATE and an OUT pin, leading to the pins GATE-0, OUT-0, GATE-1, OUT-1,
+ * and GATE-2, OUT-2.
  *
- * The 0x61 port is output port B of the 8255 peripheral interface chip.
- * Ofcourse all of this is just emulated in the mobo southbridge.
+ * If GATE-x is positive, it enables the timer x to count, otherwise the
+ * timer's count value stand still.
+ *
+ * In a PC compatible machine, GATE-0 and GATE-1 are always positive.
+ * GATE-2 is controlled by Port 0x61 bit 0.
+ *
+ * OUT-x is the output pin of timer x. OUT-0 is traditionally mapped to
+ * IRQ 0. OUT-1 was typically connected to DRAM refresh logic and is now
+ * obsolete. OUT-2 is connected to port 0x61 bit 5.
+ *
+ * NOTE! Port 0x61 bit 1 is an input pin to a glue AND logic with the
+ * OUT-2 pin, which leads to outputting counter 2 ticks to the system
+ * speaker if set.
  *
  * Delays are tested against wall clock time.
  */
@@ -30,12 +40,12 @@
 #define PIT_CLOCK_RATE	1193182ul	/* Hz (ticks per second) */
 
 /*
- * PIT-related port 0x61 bits
+ * Extract a PIT-related bit from port 0x61
  */
 enum {
-	PIT_GATE2   = 0x1,		/* PIT's GATE 2 pin */
-	PIT_SPEAKER = 0x2,		/* Speaker enabled? */
-	PIT_OUT2    = 0x20,		/* PIT's Counter 2 output pin */
+	PIT_GATE2   = 0x1,		/* bit 0 - PIT's GATE-2 input */
+	PIT_SPEAKER = 0x2,		/* bit 1 - enable/disable speaker */
+	PIT_OUT2    = 0x20,		/* bit 5 - PIT's OUT-2 */
 };
 
 /*
@@ -59,7 +69,7 @@ union pit_cmd {
 		uint8_t bcd:1,		/* BCD format for counter divisor? */
 			mode:3,		/* Counter modes 0 to 5 */
 			rw:2,		/* Read/Write latch, LSB, MSB, or 16-bit */
-			counter:2;	/* Which counter of the three (0-2) */
+			timer:2;	/* Which timer of the three (0-2) */
 	} __packed;
 };
 
@@ -84,9 +94,8 @@ enum {
 };
 
 /*
- * Start timer 2 counter: raise the PIT's GATE 2 input pin.
- * Disable the glue between timer2 output and the speaker in
- * the process.
+ * Start counter 2: raise the GATE-2 pin.
+ * Disable glue between OUT-2 and the speaker in the process
  */
 static inline void timer2_start(void)
 {
@@ -97,7 +106,7 @@ static inline void timer2_start(void)
 }
 
 /*
- * Freeze timer 2 counter by clearing the GATE2 pin.
+ * Freeze counter 2: clear the GATE-2 pin.
  */
 static inline void timer2_stop(void)
 {
@@ -106,6 +115,11 @@ static inline void timer2_stop(void)
 	val = inb(0x61) & ~PIT_GATE2;
 	outb(val, 0x61);
 }
+
+/*
+ * Did we program PIT's counter0 to monotonic mode?
+ */
+static bool timer0_monotonic;
 
 /*
  * Delay/busy-loop for @ms milliseconds.
@@ -118,17 +132,19 @@ void pit_mdelay(int ms)
 	uint32_t counter;
 	uint8_t counter_low, counter_high;
 
+	/* GATE-2 down */
 	timer2_stop();
 
 	cmd.bcd = 0;
 	cmd.mode = MODE_0;
 	cmd.rw = RW_16bit;
-	cmd.counter = 2;
+	cmd.timer = 2;
 	outb(cmd.raw, PIT_CONTROL);
 
 	/* counter = ticks per second * seconds to delay
 	 * counter = PIT_CLOCK_RATE * (ms / 1000)
 	 * We use division to avoid float arithmetic */
+	assert(ms > 0);
 	counter = PIT_CLOCK_RATE / (1000 / ms);
 
 	assert(counter <= UINT16_MAX);
@@ -138,10 +154,82 @@ void pit_mdelay(int ms)
 	outb(counter_low, PIT_COUNTER2);
 	outb(counter_high, PIT_COUNTER2);
 
+	/* GATE-2 up */
 	timer2_start();
 
 	while ((inb(0x61) & PIT_OUT2) == 0)
 		asm ("pause");
+}
+
+/*
+ * Trigger PIT IRQ pin (OUT-0) after given timeout
+ */
+void pit_oneshot(int ms)
+{
+	union pit_cmd cmd = { .raw = 0 };
+	uint32_t counter;
+	uint8_t counter_low, counter_high;
+
+	/* No control over GATE-0: it's always positive */
+
+	if (timer0_monotonic == true)
+		panic("PIT: Programming timer0 as one-shot will "
+		      "stop currently setup monotonic mode");
+
+	cmd.bcd = 0;
+	cmd.mode = MODE_0;
+	cmd.rw = RW_16bit;
+	cmd.timer = 0;
+	outb(cmd.raw, PIT_CONTROL);
+
+	/* FIXME: we may like to put this division in a
+	 * header file to let GCC do it at compile-time */
+	assert(ms > 0);
+	counter = PIT_CLOCK_RATE / (1000 / ms);
+
+	assert(counter <= UINT16_MAX);
+	counter_low = counter & 0xff;
+	counter_high = counter >> 8;
+
+	outb(counter_low, PIT_COUNTER0);
+	outb(counter_high, PIT_COUNTER0);
+}
+
+/*
+ * Let the PIT fire at monotonic rate.
+ *
+ * OUT-2 actually starts high and stands high half the period,
+ * go low the other half, then return high.
+ *
+ * Since we program the PIT IRQ as edge-triggered, this gives
+ * us the desirable effect of an IRQ every @ms since we  only
+ * notice the last rise.
+ */
+void pit_monotonic(int ms)
+{
+	union pit_cmd cmd = { .raw = 0 };
+	uint32_t counter;
+	uint8_t counter_low, counter_high;
+
+	/* No control over GATE-0: it's always positive */
+
+	timer0_monotonic = true;
+
+	cmd.bcd = 0;
+	cmd.mode = MODE_3;
+	cmd.rw = RW_16bit;
+	cmd.timer = 0;
+	outb(cmd.raw, PIT_CONTROL);
+
+	assert(ms > 0);
+	counter = PIT_CLOCK_RATE / (1000 / ms);
+
+	assert(counter <= UINT16_MAX);
+	counter_low = counter & 0xff;
+	counter_high = counter >> 8;
+
+	outb(counter_low, PIT_COUNTER0);
+	outb(counter_high, PIT_COUNTER0);
 }
 
 /*
--- include/pit.h
+++ include/pit.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 
 void pit_mdelay(int ms);
+void pit_oneshot(int ms);
+void pit_monotonic(int ms);
 uint64_t pit_calibrate_cpu(int repeat);
 
 #endif /* _PIT_H */
