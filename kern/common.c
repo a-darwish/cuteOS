0ac698d96640630d509df3318757632faf5c8dcd
Ahmed S. Darwish <darwish.07@gmail.com>
2010-11-18 03:51:49 +0200

    Panic(): Quickly disable system interrupts upon entrance!
    Disabling interrupts is an omnipotent locking mechanism from
    a single-CPU  perspective. Check  the now-committed comments
    for further rationale.

--- kern/common.c
+++ kern/common.c
@@ -12,14 +12,34 @@
 #include <idt.h>
 
 /*
- * FIXME: fix the SMP case once primitive SMP
- * code is ready
+ * Protected (for now) by disabling local interrupts.
+ *
+ * Disabling interrupts is an _omnipotent_ locking mechanism
+ * from a single-CPU perspective.
+ */
+static char buf[256];
+
+/*
+ * Quickly disable system interrupts upon entrance! Now the
+ * kernel is in an inconsistent state, just gracefully stop
+ * the machine and halt.
+ *
+ * An interesting case faced from not disabling interrupts
+ * early on (disabling them at the function end instead) was
+ * having other threads getting scheduled between printk()
+ * and disabling interrupts, scrolling-away the caller panic
+ * message and losing information FOREVER.
+ *
+ * FIXME: Handle the multi-core case. Worry about this when
+ * we're able to schedule threads on secondary CPUs.
  */
 void __no_return panic(const char *fmt, ...)
 {
 	va_list args;
-	char buf[256];
 	int n;
+
+	/* NOTE! Do not put anything above this */
+	local_irq_disable();
 
 	va_start(args, fmt);
 	n = vsnprintf(buf, sizeof(buf) - 1, fmt, args);
@@ -28,6 +48,5 @@ void __no_return panic(const char *fmt, ...)
 	buf[n] = 0;
 	printk("\nPANIC: %s", buf);
 
-	local_irq_disable();
 	halt();
 }
